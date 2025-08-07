9287b77c36e05128377d2fb72fa389c572740e6b
skrsikop <skrsikop@gmail.com>
2025-08-07 17:21:17 +0300

    demo mode 2 update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
         onClick={handleNavigation}
         className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
       >
-        {token ? 'Dashboard' : 'Login'}
+        {token ? 'Dashboard' : 'Admin Login'}
         <img src={assets.arrow} alt="arrow" className="w-3" />
       </button>
     </div>
